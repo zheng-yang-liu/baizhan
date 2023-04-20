50d71417a45278caf837471c78775b5a90d72442
thexu <thexuyan@outlook.com>
2023-04-20 10:03:54 +0800

    安卓APPbaizhan项目

---
+++ compoment/free-card/free-card.vue
@@ -0,0 +1,115 @@
+<template>
+	<view>
+		<view class="free_card_box" v-for="(item,index) in teaList" :key="index">
+			<view class="free_card_img">
+				<image :src="item.teacher_logo" mode=""></image>
+			</view>
+			<view class="free_card_txt">
+				<view class="free_card_T">{{ item.limitName }}</view>
+				<view class="free_card_info">
+					<view class="free_card_info_txt">
+						<view class="info_txt1">{{ item.teacher_name }}{{ item.teacher_job }}</view>
+						<view>{{ item.limitNum }}人学过</view>
+					</view>
+					<view class="free_card_info_btn" v-if="item.baoming == '立即学习'">{{ item.baoming }}</view>
+					<view class="free_card_info_btn free_card_info_btn1" v-else>{{ item.baoming }}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				teaList:[]
+			}
+		},
+		methods: {
+			
+		},
+		mounted(){
+			uni.request({
+				url:"http://html5.bjsxt.cn/api/index/specific?user=2162",
+				success:(res)=>{
+					this.teaList=res.data.data;
+					// console.log(res.data.data)
+				}
+			})
+		}
+	}
+</script>
+
+<style lang="scss">
+	.free_card_box{
+			display: flex;
+			padding: 10px 0;
+			margin: 10px;
+			border-radius: 10px;
+			box-shadow: 0 0  5px 1px rgba($color: #000000, $alpha: 0.1);
+			box-sizing: border-box;
+			align-items: center;
+			margin-bottom: 15px;
+			background-color: #fff;
+			
+			.free_card_img{
+				flex-shrink: 0;
+				width: 91rpx;
+				height: 91rpx;
+				border-radius: 100%;
+				margin: 0 15px;
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 100%;
+				}
+			}
+			.free_card_txt{
+				width: 100%;
+				display: flex;
+				box-sizing: border-box;
+				flex-direction: column;
+				padding: 0 15px 0 0;
+				.free_card_T{
+					font-size: 16px;
+					white-space: nowrap;
+					text-overflow: ellipsis;
+					overflow: hidden;
+					margin: 10px 0;
+				}
+				.free_card_info{
+					width: 100%;
+					display: flex;
+					box-sizing: border-box;
+					flex-flow: row nowrap;
+					justify-content: space-between;
+					.free_card_info_txt{
+						width: 60%;
+						overflow: hidden;
+						font-size: 16px;
+						color: #666;
+						.info_txt1{
+							height: 20px;
+							font-size:14px;
+							overflow: hidden;
+						}
+					}
+					.free_card_info_btn{
+						width: 100px;
+						height: 34px;
+						text-align: center;
+						line-height: 34px;
+						border-radius: 34px;
+						background-color: #00b783;
+						color: #fff;
+						font-size: 16px;
+						margin-top: 10px;
+					}
+					.free_card_info_btn1{
+						background-color: #ddd;
+					}
+				}
+			}
+		}
+</style>
---
+++ pages/tabber/index/index.vue
@@ -0,0 +1,171 @@
+<template>
+	<view class="home">
+		<Navber />
+		<!-- 
+			 indicator-dots显示指示点
+			 indicator-color指示点颜色
+			 indicator-active-color当前选中的指示点颜色
+			 autoplay是否 自动播放
+			 interval自动切换的时间
+			 duration滑动的时长
+			 -->
+		<!-- 轮播图 -->
+		<view class="index_banner_box">
+			<swiper class="swiper"  :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item v-for="(item,index) in topBanner" :key="index">
+					<image :src="item.img_url" class="banner"></image>
+					
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 课程列表 -->
+		<coursenav />
+		<!-- 限时免费-->
+		<view class="online_box">
+			<image :src="index_banner.img_url" class="online_img"></image>
+		</view>
+		
+		<view class="free_box">
+			<!-- 公共样式 -->
+			<view class="free_T_box public_tow_box">
+				<view class="public_T">
+					限时免费
+				</view>
+			</view>
+			<!-- 插入出列表；实现一个课程列表效果 -->
+			<FreeCard />
+		</view>
+		 
+	</view>
+</template>
+
+<script>
+	// 课程列表
+	import Navber from"@/uni_modules/uni-nav-bar/components/navber/navber.vue"
+	import coursenav from"@/uni_modules/coursenav/course.vue"
+	import FreeCard from"@/compoment/free-card/free-card.vue"
+	export default {
+		data() {
+			return {
+				topBanner:[],
+				index_banner:{}
+			}
+		},
+		methods: {
+			
+		},
+		mounted(){
+			console.log('打印')
+			//进行网页请求，获取数据
+			uni.request({
+				// response:响应
+				url: 'http://html5.bjsxt.cn/api/index/banner', //仅为示例，并非真实接口地址。
+				//回调函数·（访问成功后进行的操作）`
+				success: (res) => {
+					console.log(res.data);
+					// 更新数据
+					// console.log(res.data.top_banner);
+					this.topBanner=res.data.top_banner;
+					// topBnner数组元素
+					// console.log(res.data.index_banner);
+					this.index_banner=res.data.index_banner;
+				}
+			});
+		},
+		components: {
+			 Navber,
+			 coursenav,
+			 FreeCard
+		}
+	}
+</script>
+<style lang="scss">
+	.home {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		overflow: hidden;
+		.index_banner_box {
+			display: flex;
+			width: 100%;
+			padding: 10px;
+			justify-content: center;
+			align-items: center;
+			border-radius: 5px;
+			overflow: hidden;
+			.swiper{
+				width: 100%;
+				height: 260rpx;
+				.banner{
+					width: 700rpx;
+					height: 260rpx;
+				}
+			}
+		}
+		.online_box{
+			display: flex;
+			width: 724rpx;
+			justify-content: center;
+			align-items: center;
+			box-sizing: border-box;
+			overflow: hidden;
+			margin-bottom: 15px;
+			
+			.online_img{
+				width: 724rpx;
+				height: 132rpx;
+			}
+			
+		}
+		.public_tow_box{
+			display: flex;
+			width: 100%;
+			justify-content: center;
+			align-items: center;
+			box-sizing: border-box;
+			overflow: hidden;
+			padding: 0 15px;
+			justify-content: space-between;
+			align-content: space-between;
+			flex-wrap: wrap;
+			.public_T{
+				font-size: 20px;
+				font-weight: 700;
+			}
+		}
+		.public_title{
+			width: 100%;
+			display: flex;
+			padding: 0 15px;
+			flex-direction: column;
+			.public_class_t{
+				font-size: 22px;
+				font-weight: 700;
+				margin-bottom: 15px;
+			}
+		}
+		.recommed_T_box{
+			margin-bottom: 25px;
+		}
+		
+		.daotu_box{
+			display: flex;
+			box-sizing: border-box;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			
+			.daotu_T{
+				font-size: 18px;
+				font-weight: 700;
+				margin: 15px ;
+			}
+			image{
+				width: 699rpx;
+				height: 634rpx;
+				margin: 0 0 15px 0;
+			}
+		}
+	}
+</style>
+
